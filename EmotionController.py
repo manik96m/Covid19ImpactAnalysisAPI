a10abd32655a7cda92545cbc2f9928a4f2417c33
tincy0499 <71381818+tincy0499@users.noreply.github.com>
2020-10-22 19:54:38 +0530

    Emotion Analysis with controller

---
+++ EmotionController.py
@@ -0,0 +1,10 @@
+from extractTweet import *
+from calculateSentiment import *
+
+def start():
+    location = input("Enter the location to analyse tweets : ")
+    tweetEmotions(location)
+
+if __name__ == "__main__":
+   #running controller function
+   start()
