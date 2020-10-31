be67e112824d2c3bea27cab695878a9b0880878e
manik <manik96.m@gmail.com>
2020-10-31 13:34:22 +0530

    updated emotion api

--- EmotionController.py
+++ EmotionController.py
@@ -7,48 +7,34 @@ from ExtractTweet import *
 from CalculateSentiment import *
 from Canada import *
 from India import *
-from _ast import If
-from flask_cors import CORS
-import flask
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-CORS(app)
 
 class EmotionController:
     def __init__(self):
-        self.emotions = CalculateSentiments();
+        self.emotions = CalculateSentiments()
 
-    @app.route('/getScores', methods=['GET'])
-    def start(self,location = 'canada'):
+    def start(self, location='canada'):
         data = {}
 
         tweet = ExtractTweet()
-        if(location.casefold()=='canada'):
+        if(location.casefold() == 'canada'):
             hotSpots = Canada()
-        if(location.casefold()=='india'):
+        if(location.casefold() == 'india'):
             hotSpots = India()
 
         hotSpotsValues = hotSpots.getHotSpots()
 
         for uniqueHotspot in hotSpotsValues:
             locationTweet = tweet.getTweets(uniqueHotspot)
-            if(locationTweet.empty==False):
+            if(locationTweet.empty == False):
                 print(uniqueHotspot)
-                data[uniqueHotspot] = self.emotions.getScoresforTweets(uniqueHotspot,locationTweet)
+                data[uniqueHotspot] = self.emotions.getScoresforTweets(
+                    uniqueHotspot, locationTweet)
 
         print(data)
         return data
 
-    def scoreForText(self,text):
+    def scoreForText(self, text):
         data = self.emotions.getScoreForText(text)
         print(data)
         return data
-
-app.run()
-
-if __name__ == "__main__":
-   # running controller function
-   e = EmotionController()
-   e.scoreForText('This is my text and I am feeling happy to write this text but it could have been better')
-   e.start()
