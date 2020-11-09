55a2f7684d2cd8ce36f9552fcf0092442a3b6c8a
tincy0499 <tincyujodz@gmail.com>
2020-11-09 13:06:05 +0530

    Added Comments, Check Tweets <=20

--- TestEmotionController.py
+++ TestEmotionController.py
@@ -1,4 +1,12 @@
+"""
+Created on Oct 30 2020
+
+@author: tincythomas
+JUNIT test cases to caluculate scores of tweets using IBM Tone Analyser
+"""
+
 from EmotionController import *
+import unittest
 
 
 class TestEmotionController(unittest.TestCase):
@@ -7,12 +15,12 @@ class TestEmotionController(unittest.TestCase):
 
     def testTweetEmotions(self):
         data = self.e.start('Canada')
-        self.assertTrue('Delhi' in result.values())
-        self.assertTrue('fearScore' in result.keys())
+        #self.assertTrue('Ontario' in data.values()['provinceName'])
+        self.assertTrue('provinceName' in data.keys())
 
     def testTextEmotions(self):
         data = self.e.scoreForText('#COVID19 does not even spare alcoholics. So, bust this myth and stop consuming alcohol to safeguard yourself from #Coronavirus.;Delhi Better be at home and take precautionary measures to fight against #CoronavirusPandemic. ;Poetry')
-        self.assertTrue('fearScore' in result.keys())
+        self.assertTrue('text' in data.keys())
 
 if __name__ == '__main__':
     unittest.main()
