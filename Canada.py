4c438a21f553a71f7ed5f5a0c985aea48f77aeb4
tincy0499 <tincyujodz@gmail.com>
2020-11-12 23:25:59 +0530

    testing timeout

--- Canada.py
+++ Canada.py
@@ -9,7 +9,7 @@ from CovidHotSpots import CovidHotSpots
 class Canada(CovidHotSpots):
     
     def getHotSpots(self):
-        CAhotspotCSV=pd.read_csv('COVIDCANADA.csv')
+        CAhotspotCSV=pd.read_csv('COVIDCANADA1.csv')
         for column_name, column in CAhotspotCSV.transpose().iterrows():
             if(column_name=='province'):
                 CAhotspot=CAhotspotCSV[column_name].dropna().unique()
